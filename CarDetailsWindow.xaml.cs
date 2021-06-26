b74e6a2b2da6de14db076af40df2a4ca61bcc429
Konrad Dykas <w60027@student.wsiz.rzeszow.pl>
2021-06-26 09:14:31 +0200

    Dodaj pliki projektów.

---
+++ CarDetailsWindow.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CarHelper
+{
+    /// <summary>
+    /// Logika interakcji dla klasy CarDetailsWindow.xaml
+    /// </summary>
+    public partial class CarDetailsWindow : Window
+    {
+        SqlConnection connection;
+        public string carID;
+        public int rates;
+        public int votes;
+        public string sql;
+        public CarDetailsWindow(string brand, string model, string engine, string conn)
+        {
+            InitializeComponent();
+            connection = new SqlConnection(conn);
+            connection.Open();
+
+            SqlCommand cmd = new SqlCommand("select * from Cars WHERE carBrand = '"+ brand + "' AND carModel = '" + model + "' AND engineDesignation = '" + engine + "'", connection);
+            SqlDataReader sdr = cmd.ExecuteReader();
+
+            while (sdr.Read())
+            {
+                titleLabel.Content += sdr[1] + " " + sdr[2] + " " + sdr[4] + "  (" + sdr[13] + " - " + sdr[14] + ")";
+                bodyLabel.Content += " " + sdr[3];
+                capacityLabel.Content += " " + sdr[5];
+                horseLabel.Content += " " + sdr[6];
+                torqueLabel.Content += " " + sdr[7];
+                accLabel.Content += " " + sdr[8];
+                speedLabel.Content += " " + sdr[9];
+                fuelLabel.Content += " " + sdr[10];
+                oilLabel.Content += " " + sdr[11];
+                carID += sdr[0];
+            }
+            sdr.Close();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            SqlCommand cmdRate = new SqlCommand("SELECT rate, amountOfvotes FROM carsRating WHERE versionID="+ Convert.ToInt32(carID) + "", connection);
+            SqlDataReader sdr = cmdRate.ExecuteReader();
+            while (sdr.Read())
+            {
+                rates = Convert.ToInt32(sdr[0]);
+                votes = Convert.ToInt32(sdr[1]);
+            }
+            sdr.Close();
+            votes += 1;
+
+            if(rad1.IsChecked == true)
+            {
+                rates += Convert.ToInt32(rad1.Content);
+            }
+            else if (rad2.IsChecked == true)
+            {
+                rates += Convert.ToInt32(rad2.Content);
+            }
+            else if (rad3.IsChecked == true)
+            {
+                rates += Convert.ToInt32(rad3.Content);
+            }
+            else if (rad4.IsChecked == true)
+            {
+                rates += Convert.ToInt32(rad4.Content);
+            }
+            else
+            {
+                rates += Convert.ToInt32(rad5.Content);
+            }
+
+            if(votes == 1)
+            {
+                sql = "INSERT INTO carsRating (versionID, rate, amountOfvotes)  VALUES (" + carID + ", " + rates + ", " + votes + ")";
+            }
+            else
+            {
+                sql = "UPDATE carsRating SET rate=" + rates + ", amountOfvotes=" + votes + " WHERE versionID=" + carID + "";
+            }
+            SqlCommand cmdAddRate = new SqlCommand(sql , connection);
+            cmdAddRate.ExecuteNonQuery();
+            rateBtn.IsEnabled = false;
+
+        }
+
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CarHelper
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static string conn = "Server=KONRAD\\SQLEXPRESS;Database=CarHelper;Integrated Security=true;";
+        public string brand;
+        public string model;
+        public string engine;
+
+        SqlConnection connection;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+
+            connection = new SqlConnection(conn);
+            connection.Open();
+            ratedEngines.Text += Environment.NewLine + Environment.NewLine;
+            SqlCommand cmdRate = new SqlCommand("SELECT carBrand, carModel, engineDesignation, (rate/amountOfvotes) FROM carsRating INNER JOIN Cars ON carsRating.versionID = Cars.CarID order by (rate/amountOfvotes) DESC", connection);
+            SqlDataReader sdrRate = cmdRate.ExecuteReader();
+            int a = 1;
+            while (sdrRate.Read())
+            {
+                ratedEngines.Text += a + ". " + sdrRate[0] + " " + sdrRate[1] + " " + sdrRate[2] + " :      " + sdrRate[3] + "/5" + Environment.NewLine;
+                a++;
+            }
+            sdrRate.Close();
+
+            SqlCommand cmd = new SqlCommand("SELECT carBrand FROM Cars group by carBrand", connection);
+            SqlDataReader sdr = cmd.ExecuteReader();
+
+            while (sdr.Read())
+            {
+                brandBox.Items.Add(sdr[0]);
+            }
+            sdr.Close();
+        }
+
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            brand = brandBox.Text;
+            modelBox.IsEnabled = true;
+            modelBtn.IsEnabled = true;
+            SqlCommand cmd1 = new SqlCommand("SELECT carModel FROM Cars WHERE carBrand='"+ brand +"' group by carModel", connection);
+            SqlDataReader sdr1 = cmd1.ExecuteReader();
+            while (sdr1.Read())
+            {
+                modelBox.Items.Add(sdr1[0]);
+            }
+            sdr1.Close();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            model = modelBox.Text;
+            engineBox.IsEnabled = true;
+            engineBtn.IsEnabled = true;
+            SqlCommand cmd1 = new SqlCommand("SELECT engineDesignation FROM Cars WHERE carModel='" + model + "'", connection);
+            SqlDataReader sdr1 = cmd1.ExecuteReader();
+            while (sdr1.Read())
+            {
+                engineBox.Items.Add(sdr1[0]);
+            }
+            sdr1.Close();
+        }
+
+        private void engineBtn_Click(object sender, RoutedEventArgs e)
+        {
+            engine = engineBox.Text;
+            var CarDetailsWindow = new CarDetailsWindow(brand, model, engine, conn);
+            CarDetailsWindow.Show();
+            modelBox.Items.Clear();
+            engineBox.Items.Clear();
+            engineBox.IsEnabled = false;
+            engineBtn.IsEnabled = false;
+            modelBox.IsEnabled = false;
+            modelBtn.IsEnabled = false;
+        }
+    }
+}
